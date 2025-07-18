e2b4a807eaa9622312b26ee55db6bacd04f1413b
Pankaj Singh <95837801+itspunkaj@users.noreply.github.com>
2025-07-18 20:25:17 +0530

    refactor: update hero section of advertisements (#1)

--- app/home/advertising/components/main.tsx
+++ app/home/advertising/components/main.tsx
@@ -1,5 +1,5 @@
 import About from "./about-us";
-import HeroSection from "./hero-section";
+import HeroSection from "./hero-section-2";
 import CounterSection from "./metric-section";
 import ProjectsSection from "./projects";
 import QuoteSection from "./quote";
